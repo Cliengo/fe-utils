1b0f33bcd360994e86eb6af6129d971f1fb5703e
Jorge Figueroa <jorge.figueroa@cliengo.com>
2022-06-06 08:52:03 -0600

    Adding typescript support for analytics

--- index.d.ts
+++ index.d.ts
@@ -1,4 +1,50 @@
 declare module '@cliengo/fe-utils' {
-  export function copyToClipboard(value: string): boolean;
-  export function isEmpty(value: any): boolean;
+  function copyToClipboard(value: string): boolean;
+  function isEmpty(value: any): boolean;
+  interface IAccount {
+    name: string;
+    planName: string;
+    id: string;
+    whiteLabelId?: string;
+    vertical?: string;
+    registerRole?: string;
+    leadCount?: number;
+    labs?: { [key: string]: boolean };
+    language?: string;
+    countryId?: string;
+  }
+
+  interface IUser {
+    id: string;
+    email?: string;
+    privileges?: string;
+  }
+
+  interface IAnalyticsInitializeParams {
+    segmentWriteKey: string;
+    sendAnalytics: string;
+    account: IAccount;
+    user: IUser;
+  }
+
+  interface ITrackEventProps {
+    eventName: string;
+    category?: string;
+    action?: string;
+    label?: string;
+  }
+
+  class Analytics {
+    constructor(sendAnalytics: string, initialized: string);
+
+    public static initialize(params: IAnalyticsInitializeParams): void;
+    public static trackEvent(eventProps: ITrackEventProps): void;
+    public static trackPage(path: string, websiteId: string, url: string): void;
+  }
+
+  export {
+    Analytics,
+    copyToClipboard,
+    isEmpty,
+  }
 }
